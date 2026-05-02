64faa5c3342dcf6521c5ac182bf7425f04fddce0
Kevin-Loran <kevinloran.eng@gmail.com>
2026-05-02 17:16:10 -0300

    feat: lista-de-tarefas/Estilizando tarefas.

--- Mini-Projetos/lista-de-tarefas/src/components/main.js
+++ Mini-Projetos/lista-de-tarefas/src/components/main.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 // form
-import { FaPlus } from 'react-icons/fa'
+import { FaPlus} from 'react-icons/fa'
+
+// taefa
+import { FaEdit, FaWindowClose   } from 'react-icons/fa'
 
 import './main.css'
 
 export default class Main extends Component {
         state = {
             novatarefa: '',
+            tarefas: 
+           [ 'fazer cafe',
+            'estudar',
+            'tomar banho']
         };
 
 
@@ -18,6 +25,8 @@ export default class Main extends Component {
     }
 
     render() {
+        const { novatarefa, tarefas } = this.state
+
         return (
             <div className="main">
                 <h1>Lista de tarefas</h1>
@@ -27,6 +36,18 @@ export default class Main extends Component {
                     <FaPlus/>
                     <button type='submit'>Enviar</button>
                 </form>
+
+                <ul className="tarefas">
+                   {tarefas.map((tarefa) => {
+                   return  <li key={tarefa}>
+                    { tarefa }
+                    <div>
+                        <FaEdit className="edit"/>
+                        <FaWindowClose className="delete"/>
+                    </div>
+                    </li>
+                   })}
+                </ul>
             </div>
         );
     }
