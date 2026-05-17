4b09c0ebb4ea031a7e6a574d0a184a0c7808c0a9
Kevin-Loran <kevinloran.eng@gmail.com>
2026-05-17 15:41:28 -0300

    feat: react-base/Configurando o reduxPersist.

--- Mini-Projetos/react-base/src/App.js
+++ Mini-Projetos/react-base/src/App.js
@@ -1,22 +1,25 @@
-import Routes from "./Routes";
+import { Router } from "react-router-dom/cjs/react-router-dom.min";
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from './store';
+import store, { persistor } from './store';
 import history from "./services/history";
 import GlobalStyles from "./styles/GlobalStyles";
 import Header from "./components/Header";
-import { Router } from "react-router-dom/cjs/react-router-dom.min";
+import Routes from "./Routes";
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history} >
-        <GlobalStyles />
-        <ToastContainer autoClose={3000} className="toast-container"/>
-        <Header />
-        <Routes />
-      </Router >
+      <PersistGate persistor={persistor}>
+        <Router history={history} >
+          <GlobalStyles />
+          <ToastContainer autoClose={3000} className="toast-container"/>
+          <Header />
+          <Routes />
+        </Router >
+      </PersistGate>
     </Provider>
   );
 }
